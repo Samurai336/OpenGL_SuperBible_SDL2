2b018b553deabddd6ad015acd132882ad060f56a
Eric <samurai336@gmail.com>
2014-01-11 11:44:53 -0500

    Does tesslated triangle Watch out for the render call chane not in the book!

--- src/Renderer/GLRenderer.cpp
+++ src/Renderer/GLRenderer.cpp
@@ -52,9 +52,17 @@ bool GLRenderer::InitRenderer(SDL_Window* windowToRenderTo)
 void GLRenderer::startup()
 {
 
+
     rendering_program = compile_shaders();
+
+
     glGenVertexArrays(1, &vertex_array_object);
     glBindVertexArray(vertex_array_object);
+
+    glPatchParameteri(GL_PATCH_VERTICES, 3);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+
 
 }
 
@@ -64,6 +72,8 @@ GLuint GLRenderer::compile_shaders()
 {
     GLuint vertex_shader;
     GLuint fragment_shader;
+    GLuint tesselation_Control_shader;
+    GLuint tesselation_Eval_shader;
     GLuint program;
 
 
@@ -101,7 +111,7 @@ GLuint GLRenderer::compile_shaders()
         "in VS_OUT                                  \n"
         "{                                          \n"
         "   vec4 color;                             \n"
-        "}fs_in;                                   \n"
+        "}fs_in;                                    \n"
         "                                           \n"
         "                                           \n"
         "out vec4 color;                            \n"
@@ -112,11 +122,57 @@ GLuint GLRenderer::compile_shaders()
         "}                                          \n"
     };
 
+    static const GLchar * tesselation_Control_shader_source[] =
+    {
+        "#version 410 core                          \n"
+        "                                           \n"
+        "layout (vertices = 3) out;                 \n"
+        "                                           \n"
+        "                                           \n"
+        "void main(void)                            \n"
+        "{                                          \n"
+        "    if(gl_InvocationID == 0)               \n"
+        "    {                                      \n"
+        "         gl_TessLevelInner[0] = 5.0;       \n"
+        "         gl_TessLevelOuter[0] = 5.0;       \n"
+        "         gl_TessLevelOuter[1] = 5.0;       \n"
+        "         gl_TessLevelOuter[2] = 5.0;       \n"
+        "    }                                       \n"
+        "                                           \n"
+        "    gl_out[gl_InvocationID].gl_Position = gl_in[gl_InvocationID].gl_Position;      \n"
+        "}                                          \n"
+
+    };
+
+     static const GLchar * tesselation_Eval_shader_source[] =
+    {
+        "#version 410 core                          \n"
+        "                                           \n"
+        "layout (triangles, equal_spacing, cw) in;  \n"
+        "                                           \n"
+        "                                           \n"
+        "void main(void)                            \n"
+        "{                                          \n"
+        "     gl_Position = (gl_TessCoord.x * gl_in[0].gl_Position) + \n"
+        "                   (gl_TessCoord.y * gl_in[1].gl_Position) + \n"
+        "                   (gl_TessCoord.z * gl_in[2].gl_Position); \n"
+        "                                                            \n"
+        "}                                                           \n"
+    };
+
 
     //Compile the Vertex shader
     vertex_shader = glCreateShader(GL_VERTEX_SHADER);
     glShaderSource(vertex_shader, 1, vertex_shader_source, NULL);
     glCompileShader(vertex_shader);
+
+    GLint vShaderCompiled = GL_FALSE;
+    glGetShaderiv( vertex_shader, GL_COMPILE_STATUS, &vShaderCompiled );
+    if( vShaderCompiled != GL_TRUE )
+    {
+        printf( "Unable to compile Vertex shader %d!\n", vertex_shader );
+        printShaderLog( vertex_shader );
+    }
 
 
     //Compile Frag shader
@@ -124,18 +180,66 @@ GLuint GLRenderer::compile_shaders()
     glShaderSource(fragment_shader, 1, fragment_shader_source, NULL);
     glCompileShader(fragment_shader);
 
+    GLint fShaderCompiled = GL_FALSE;
+    glGetShaderiv( fragment_shader, GL_COMPILE_STATUS, &fShaderCompiled );
+    if( fShaderCompiled != GL_TRUE )
+    {
+        printf( "Unable to compile fragment shader %d!\n", fragment_shader );
+        printShaderLog( fragment_shader );
+    }
+
+    tesselation_Control_shader = glCreateShader(GL_TESS_CONTROL_SHADER);
+    glShaderSource(tesselation_Control_shader, 1 , tesselation_Control_shader_source, NULL);
+    glCompileShader(tesselation_Control_shader);
+
+    GLint tcShaderCompiled = GL_FALSE;
+    glGetShaderiv( tesselation_Control_shader, GL_COMPILE_STATUS, &tcShaderCompiled );
+    if( tcShaderCompiled != GL_TRUE )
+    {
+        printf( "Unable to compile Tesslaton Control shader %d!\n", tesselation_Control_shader );
+        printShaderLog( tesselation_Control_shader );
+    }
+
+    tesselation_Eval_shader = glCreateShader(GL_TESS_EVALUATION_SHADER);
+    glShaderSource(tesselation_Eval_shader, 1, tesselation_Eval_shader_source, NULL);
+    glCompileShader(tesselation_Eval_shader);
+
+    GLint teShaderCompiled = GL_FALSE;
+    glGetShaderiv( tesselation_Eval_shader, GL_COMPILE_STATUS, &teShaderCompiled );
+    if( teShaderCompiled != GL_TRUE )
+    {
+        printf( "Unable to compile Tesslaton Evaluation shader %d!\n", tesselation_Eval_shader );
+        printShaderLog( tesselation_Eval_shader );
+    }
 
     //Create shader program ;
 
     program = glCreateProgram();
 
-    glAttachShader(program,vertex_shader);
+    glAttachShader(program, vertex_shader);
     glAttachShader(program, fragment_shader);
+    glAttachShader(program, tesselation_Control_shader);
+    glAttachShader(program, tesselation_Eval_shader);
 
     glLinkProgram(program);
 
+    GLint programSuccess = GL_TRUE;
+    glGetProgramiv( program, GL_LINK_STATUS, &programSuccess );
+    if( programSuccess != GL_TRUE )
+    {
+        printf( "Error linking program %d!\n", program );
+        printProgramLog( program );
+    }
+
+
     glDeleteShader(vertex_shader);
     glDeleteShader(fragment_shader);
+    glDeleteShader(tesselation_Control_shader);
+    glDeleteShader(tesselation_Eval_shader);
+
+
+
+
 
     return program;
 
@@ -166,6 +270,9 @@ void GLRenderer::Render(float currentTime)
 
     glPointSize(40.0f);
 
+    //
+
+
     glUseProgram(rendering_program);
 
     GLfloat attrib[] = { (float) sin(currentTime) * 0.5f,
@@ -179,7 +286,10 @@ void GLRenderer::Render(float currentTime)
     glVertexAttrib4fv(1,triColor);
 
 
-    glDrawArrays(GL_TRIANGLES, 0,3);
+
+    //He fails to mention in the book to switch to patches.
+    glDrawArrays(GL_PATCHES, 0, 3);
+    //glDrawArrays(GL_TRIANGLES, 0, 3);
 
 
 /*************************************************************************/
